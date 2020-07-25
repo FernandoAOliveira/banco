9dceda56d80aa0510776f475b540c6e0fddec990
fernandoAOliveira <fernandous4@yahoo.com.br>
2020-07-25 12:58:28 -0300

    editando model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
-end
+    has_secure_password
+    has_one :account
+  
+    validates :name, presence: true, length: { maximum: 50 }
+  
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
+  
+    after_create :init_account
+  
+    GENDER_TYPES = %w[Masculino Feminino].freeze
+  
+    private
+  
+    def init_account
+      Account.create(user: self)
+    end
+  end
