a7dd6fddc7d01f6522b891f92d6b39260d6d1c03
fernandoAOliveira <fernandous4@yahoo.com.br>
2020-07-25 13:06:26 -0300

    editando editando migrate/statements

--- db/migrate/20200725153132_create_statements.rb
+++ db/migrate/20200725153132_create_statements.rb
@@ -1,8 +1,8 @@
-class CreateStatements < ActiveRecord::Migration[6.0]
+class CreateStatements < ActiveRecord::Migration[5.1]
   def change
     create_table :statements do |t|
       t.float :balance
-      t.references :account, null: false, foreign_key: true
+      t.references :account, foreign_key: true
 
       t.timestamps
     end
