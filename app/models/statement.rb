9d6f6b6e002887f8fe9975a21f4678989d8d03ba
fernandoAOliveira <fernandous4@yahoo.com.br>
2020-07-25 12:59:15 -0300

    editando model statment

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,3 +1,7 @@
 class Statement < ApplicationRecord
   belongs_to :account
+
+  def self.get_report(date = {})
+    Statement.where('created_at >= ? and created_at <= ?', date['begin'], date['end'])
+  end
 end
